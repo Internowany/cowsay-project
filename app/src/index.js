5d749b855f4f6d75e026c99e5917f7442e8f8336
Sebastian <seba@SEBA-PC>
2025-02-04 11:11:02 +0100

    [patch] v4.0.4

--- app/src/index.js
+++ app/src/index.js
@@ -40,7 +40,7 @@ app.get('/', function (req, res) {
     var $ = cheerio.load(body);
     const st = $('h2', '#quote').text().replace(/\"/g, '').replace(/\_/g, '')
     const w = `
-Deepak Chopra Quote:
+v4.0.4 Deepak Chopra Quote:
 
 " ${st}"
 
